3fa6cf51c782437d8ad8a09516fd2d0b53881499
Sam Hart <hartsn@gmail.com>
2023-12-11 17:09:38 -0500

    imma show you how to make a poundcake

--- clzparse.py
+++ clzparse.py
@@ -6,24 +6,41 @@ import click
 import xml.etree.ElementTree as ET
 
 
-def count_for_year(gamelist, year):
-    count = 0
+def system_breakdown(gamelist):
+    systems = {}
+    for i in gamelist:
+        plat = i.find("platform/displayname").text
+        if plat in systems:
+            systems[plat] += 1
+        else:
+            systems[plat] = 1
+
+    print(f"\n>> Total number of games per system:")
+    for k in systems.keys():
+        print(f"\t{k}\t\t{systems[k]}")
+
+
+def count_for_year(gamelist, year, system):
+    updated_list = []
 
     for i in gamelist:
         added = i.find("addeddate/timestamp").text
         if added:
             add_date = datetime.datetime.fromtimestamp(int(added))
             if add_date.year == year:
-                count += 1
+                updated_list.append(i)
 
-    print(f"\n>> Total number of games in the year {year}: {count}")
+    print(f"\n>> Total number of games in the year {year}: {len(updated_list)}")
+    if system:
+        system_breakdown(updated_list)
 
 
 @click.command()
 @click.option('-y', '--year', type=int, default=None)
 @click.option('-c', '--count', is_flag=True, default=False)
+@click.option('-s', '--system', is_flag=True, default=None)
 @click.argument('filename', type=str, default=None)
-def main(year, count, filename):
+def main(year, count, system, filename):
     if filename is None:
         filename = sys.stdin
 
@@ -33,9 +50,11 @@ def main(year, count, filename):
 
     if count:
         print(f"\n>> Total number of games in collection: {len(gamelist)}")
+        if system:
+            system_breakdown(gamelist)
 
     if year:
-        count_for_year(gamelist, year)
+        count_for_year(gamelist, year, system)
 
 
 if __name__ == "__main__":
